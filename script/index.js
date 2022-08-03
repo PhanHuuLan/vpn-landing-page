b2fee372f112db830dce7a98762e131751343749
Phan Huu Lan <lanhuu0603@gmail.com>
2022-08-03 17:39:59 +0700

    slider vnp landing page

---
+++ script/index.js
@@ -0,0 +1,8 @@
+$('.customer-list').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  dots: true,
+  prevArrow: `<button type='button' class='slick-prev pull-left'><img src="./image/arow-left.png"/></button>`,
+  nextArrow: `<button type='button' class='slick-next pull-right'><img src="./image/arow-right.png"/></button>`,
+});
